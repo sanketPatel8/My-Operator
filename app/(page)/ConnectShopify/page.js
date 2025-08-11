4fbf2a4b413f9573e28b08714d49b20a51aaa00a
sanketPatel8 <sanket.xceptive@gmail.com>
2025-08-11 17:37:33 +0530

    update

--- app/(page)/ConnectShopify/page.js
+++ app/(page)/ConnectShopify/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-export default function ConnectShopify() {
+export default function ConnectShopif() {
   const router = useRouter();
   const [StoreName, setStoreName] = useState("");
 
