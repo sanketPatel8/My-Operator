87edd3e229e4c05fcf5ecae906b0ddcdd27fdc22
NevilXceptive <nevilxceptive@gmail.com>
2025-09-01 16:14:40 +0530

    update

--- app/api/shopify/orders/route.js
+++ app/api/shopify/orders/route.js
@@ -330,7 +330,7 @@ export async function POST(req) {
         function getMappedValue(mappingField, data) {
           switch (mappingField) {
             case 'Name':
-              return data.customer?.first_name || 'Customer';
+              return data.billing_address?.first_name || 'Customer';
             case 'Order id':
               return String(data?.id || '123456');
             case 'Phone number':
