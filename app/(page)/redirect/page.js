b2f95a390396ab0004ca0c3957566215db671f43
NevilXceptive <nevilxceptive@gmail.com>
2025-09-09 17:55:55 +0530

    update

--- app/(page)/redirect/page.js
+++ app/(page)/redirect/page.js
@@ -1,13 +1,13 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
 
 export default function RedirectPage() {
-  const searchParams = useSearchParams();
+  
 
   useEffect(() => {
-    const url = searchParams.get('url');
+    const params = new URLSearchParams(window.location.search);
+    const url = params.get("url");
 
     if (url) {
       window.location.href = url;
