47497b27c2b4bf922957d6821a7c75671a88ea8a
NevilXceptive <nevilxceptive@gmail.com>
2025-09-09 18:00:17 +0530

    update

--- app/(page)/redirect/page.js
+++ app/(page)/redirect/page.js
@@ -3,17 +3,14 @@
 import { useEffect } from 'react';
 
 export default function RedirectPage() {
-  
-const params = new URLSearchParams(window.location.search);
-const url = params.get("url");
   useEffect(() => {
-    
-    
+    const params = new URLSearchParams(window.location.search);
+    const url = params.get('url');
 
     if (url) {
       window.location.href = url;
     }
-  }, [url]);
+  }, []);
 
   return null;
 }
