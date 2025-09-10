ad000ef6dc4691f6477e36ec1bd54a289621a96b
sanketPatel8 <sanket.xceptive@gmail.com>
2025-09-10 23:04:45 +0530

    update

--- app/(page)/order-conformation/page.js
+++ app/(page)/order-conformation/page.js
@@ -1,7 +1,14 @@
 // This is a server component wrapper
 
+// app/order-conformation/page.tsx
+
 import OrderConfirmationClient from "@/component/OrderConfirmationClient";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <OrderConfirmationClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OrderConfirmationClient />
+    </Suspense>
+  );
 }
